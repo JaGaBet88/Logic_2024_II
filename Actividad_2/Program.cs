6f5dfd149d32806bf440f636d2cc687ce5d98d81
Jairo <jairogallegobetancur25@gmail.com>
2024-09-04 21:37:30 -0500

    Guardar cambios

---
+++ Actividad_2/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //2. Lea la longitud de tres segmentos y que muestre si se puede construir un triángulo con estos segmentos.
+
+        double seg1, seg2, seg3;
+        bool istriangle;
+        string msj;
+
+        //Pedir los datos:
+
+        Console.Write("Ingrese el primer segmento: ");
+        seg1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo segmento: ");
+        seg2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el tercer segmento: ");
+        seg3 = Convert.ToDouble(Console.ReadLine());
+
+        //Proceso de validación
+
+        if ((seg1 + seg2) > seg3)
+        {
+            if ((seg1 + seg3) > seg2)
+            {
+                if ((seg2 + seg3) > seg1)
+                {
+                    istriangle = true;
+                }
+            }
+        }
