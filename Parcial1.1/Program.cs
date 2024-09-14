ecf165ea0188201d7a0308cbe8af235ef0f8f723
Jairo <jairogallegobetancur25@gmail.com>
2024-09-13 21:11:29 -0500

    Fin actividad 1

--- Parcial1.1/Program.cs
+++ Parcial1.1/Program.cs
@@ -4,19 +4,9 @@
     {
         //Punto 1:Un vendedor recibe un sueldo base de $XXXX. Además, recibe una comisión del 10% sobre el valor total de cada venta que realiza. El vendedor desea saber:
 
-        //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes?
-        //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
-        //¿Cuál fue la venta que le generó la mayor comisión?
-        //¿Cuál fue el promedio de las comisiones que recibió por cada venta?
-
-        //Además, la empresa da un beneficio extra de $100.000 si el vendedor supera el objetivo del mes.
-        // El objetivo es sumar en las 3 ventas como mínimo $1.000.000 en ventas.
-
-        //Mostrar un mensaje donde indique si ganó o no el beneficio.
-
         const double sb = 2000000; // sb: Sueldo base
         const double porc = 0.1; // porc: Porcentaje de comision por venta.
-        double venta1, venta2, venta3, comi1, comi2, comi3, sumacomisiones,SueldoNetoMes;
+        double venta1, venta2, venta3, SumaVentas, comi1, comi2, comi3, sumacomisiones, SueldoNetoMes, PromComi, TotalPago;
         int VentaMayor;
 
         Console.Write("Ingrese el valor de la venta #1: ");
@@ -31,14 +21,17 @@
         venta3 = Convert.ToDouble(Console.ReadLine());
         comi3 = venta3 * porc;
 
-        sumacomisiones = comi1 + comi2 + comi3; 
+        //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes?
+        sumacomisiones = comi1 + comi2 + comi3;
+        //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
         SueldoNetoMes = sb + sumacomisiones;
 
-        if (venta1 > venta2 && venta1 > venta3)
+        //¿Cuál fue la venta que le generó la mayor comisión?
+        if (comi1 > comi2 && comi1 > comi3)
         {
             VentaMayor = Convert.ToInt32(venta1);
         }
-        else if (venta2 > venta1 && venta2 > venta3)
+        else if (comi2 > comi1 && comi2 > comi3)
         {
             VentaMayor = Convert.ToInt32(venta2);
         }
@@ -47,7 +40,26 @@
             VentaMayor = Convert.ToInt32(venta3);
         }
 
+        //¿Cuál fue el promedio de las comisiones que recibió por cada venta?
+        PromComi = sumacomisiones / 3;
 
+        //Además, la empresa da un beneficio extra de $100.000 si el vendedor supera el objetivo del mes.
+        // El objetivo es sumar en las 3 ventas como mínimo $1.000.000 en ventas.
+
+        //Mostrar un mensaje donde indique si ganó o no el beneficio.
+
+        SumaVentas = venta1 + venta2 + venta3;
+
+        if (SumaVentas >= 1000000)
+        {
+            TotalPago = SueldoNetoMes + 100000;
+            Console.Write($"¡Felicidades! cumpliste con la meta mensual de ventas, has ganado un beneficio económico adicional de $ {100000:C0}");
+        }
+        else
+        {
+            TotalPago = SueldoNetoMes;
+            Console.Write($"¡Ups! no cumpliste con la meta mensual de ventas este mes, no ganaste el beneficio.");
+        }
 
 
 
