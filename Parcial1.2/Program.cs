003bbb8c9d0ef13eab44f8fddebc302a533cebde
Jairo <jairogallegobetancur25@gmail.com>
2024-09-13 21:37:51 -0500

    Solicitud de coeficientes

--- Parcial1.2/Program.cs
+++ Parcial1.2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Escriba un programa que pida los coeficientes de una ecuación de primer grado (ax + b = 0) y muestre la solución.
+        // Una ecuación de primer grado puede no tener solución, tener una solución única, o que todos los números sean solución. La fórmula de las soluciones es x = -b / a
+
+        double a, b, solucion; //Declaración de variables
+
+        //Solicitar los coeficientes de la ecuacion
+        Console.Write("Ingrese el valor de 'a' (coeficiente de x): ");
+        a = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Ingrese el valor de 'b' (constante): ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+
+    }
+}
