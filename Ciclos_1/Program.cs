b0b4fcf0f7eb6076a7afa53886ee01169addab68
Jairo <jairogallegobetancur25@gmail.com>
2024-09-25 21:23:18 -0500

    Primer commit

---
+++ Ciclos_1/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Construir un algoritmo que pida n numeros por pantalla, cuando el usuario ingrese un numero negativo, el programa debe finalizar y mostrar en consola la cantidad total de números positivos que ingresó hasta antes de ingresarse el número negativo.*/
+
+        //Datos de entrada
+        int contador = 0, N_Num;
+        bool flag = true;
+
+        //Proceso
+        while (flag)
+        {
+            Console.Write("Ingrese un número: ");
+            N_Num = Convert.ToInt32(Console.ReadLine());
+
+            if (N_Num >= 0)
+            {
+                //Significa que es positivo
+                /*contador = contador + 1; // Forma clásica pero obsoleta*/
+                contador++; // Forma abreviada
+            }
+            else
+            {
+                flag = false;
+            }
+
+        
+        }
+
+    }
+}
