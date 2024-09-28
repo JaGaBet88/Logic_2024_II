8e988e1fccc9359bb2986cbda80d378fb796ebae
Jairo <jairogallegobetancur25@gmail.com>
2024-09-27 21:15:21 -0500

    Finish commit

--- Ciclos_2/Program.cs
+++ Ciclos_2/Program.cs
@@ -2,6 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        
+        /*Programe un algoritmo que pida al usuario 2 números por pantalla y que muestre todos los números comprendidos entre ambos números incluyendolos.*/
+
+        //Datos de entrada:
+        int num1, num2, menor, mayor;
+
+        Console.Write("Ingrese el primero número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 < num2)
+        {
+            menor = num1;
+            mayor = num2;
+        }
+        else 
+        {
+            menor = num2;
+            mayor = num1;
+        }
+
+        for (int i = menor; i <= mayor; i++)
+        {
+            Console.Write($"{i} ");
+        }
     }
 }
