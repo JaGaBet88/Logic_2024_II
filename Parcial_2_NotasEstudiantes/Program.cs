ff50f137beeb1f0733f6626895a5f0350be6547c
Jairo <jairogallegobetancur25@gmail.com>
2024-10-07 21:33:44 -0500

    Solicitu cantidad de notas y validaciones

--- Parcial_2_NotasEstudiantes/Program.cs
+++ Parcial_2_NotasEstudiantes/Program.cs
@@ -15,6 +15,26 @@
 
             //Solicitar la cantidad de estudiantes
             Console.WriteLine("Ingrese la cantidad de estudiantes: ");
+            while (!int.TryParse(Console.ReadLine(), out cantidadEstudiantes) || cantidadEstudiantes <= 0)
+            {
+                Console.WriteLine("Por favor, ingrese un número válido de estudiantes (mayor que 0): ");
+            }
+
+            //Procesar las notas de los estudiantes
+            for (int i = 1; i <= cantidadEstudiantes; i++)
+            {
+                double sumaNotas = 0;
+                double promedio = 0;
+                int cantidadNotas = 0;
+
+                Console.WriteLine($"Estudiante {i}: Ingrese la cantidad de notas: ");
+                while (!int.TryParse(Console.ReadLine(),out cantidadNotas) || cantidadNotas <= 0)
+                {
+                    Console.WriteLine("Por favor, ingrese una cantidad valida de notas (Mayor que 0): ");
+                }
+
+                //Ingresar las notas y calcular la suma
+            }
 
         }
     }
