87909ed0a45d8cdad4f4259a087f0d45dfcfa8ee
Jairo <jairogallegobetancur25@gmail.com>
2024-10-07 21:56:22 -0500

    Finalización

--- Parcial_2_NotasEstudiantes/Program.cs
+++ Parcial_2_NotasEstudiantes/Program.cs
@@ -2,6 +2,19 @@
 {
     private static void Main(string[] args)
     {
+
+        /* Sistema de evaluación de notas de estudiantes:
+         * El programa permite ingresar las notas de varios estudiantes, calcular su promedio y luego clasificar a cada estudiante en diferentes categorías de desempeño académico. Al final, se le pregunta al usuario si quiere ingresar otro grupo de estudiantes.
+           
+        Reglas:
+
+          - Si el promedio del estudiante es mayor o igual a 4.5, está en la categoría Excelente.
+          - Si el promedio está entre 4.0 y 4.4, está en la categoría Sobresaliente.
+          - Si el promedio está entre 3.5 y 3.9, está en la categoría Bueno.
+          - Si el promedio es menor a 3.5, está en la categoría Insuficiente.
+
+         Adicional, contar cuántos estudiantes ganaron y cuántos perdieron.*/
+
         bool nuevoGrupo = true;
 
         while (nuevoGrupo)
