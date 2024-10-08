31a2258532d18736eba4a668f2f250c824e72e7d
Jairo <jairogallegobetancur25@gmail.com>
2024-10-07 21:27:43 -0500

    Creación de variables e ingreso de datos

--- Parcial_2_NotasEstudiantes/Program.cs
+++ Parcial_2_NotasEstudiantes/Program.cs
@@ -2,6 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        
+        bool nuevoGrupo = true;
+
+        while (nuevoGrupo)
+        {
+            Console.Clear(); //Limpiar consola al comenzar un nuevo grupo
+            Console.WriteLine("Sistema de evaluación de notas de estudiantes");
+
+            int cantidadEstudiantes = 0;
+            int estudiantesGanaron = 0;
+            int estudiantesPerdieron = 0;
+
+            //Solicitar la cantidad de estudiantes
+            Console.WriteLine("Ingrese la cantidad de estudiantes: ");
+
+        }
     }
 }
