6782a4b037ecd6ea5f58b37e35b0dfc5b603fa29
Jairo <jairogallegobetancur25@gmail.com>
2024-10-07 20:32:11 -0500

    Primer progreso

--- Parcial_2_Juego_AdivinaElNumero/Program.cs
+++ Parcial_2_Juego_AdivinaElNumero/Program.cs
@@ -3,15 +3,15 @@
     private static void Main(string[] args)
     {
         /* Juego:
-         * Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en memoria sin ser revelado.
+         * Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en adivinar un número N aleatorio que el sistema escoge de un determinado rango y lo guarda en memoria sin ser revelado.
 
-         * Por ejemplo: El sistema escoge aleatoriamente un número entre 0 y 100, ese número NO SE PUEDE REVELAR ya que es el número que se adivinará, por ende, dicho número se almacenará en memoria. Los participantes comenzarán en orden a jugar y se turnarán para ingresar por pantalla un número ℕ hasta acertar el número oculto y salir ganador.
+         * Por ejemplo: El sistema escoge aleatoriamente un número entre 0 y 100, ese número NO SE PUEDE REVELAR ya que es el número que se adivinará, por ende, dicho número se almacenará en memoria. Los participantes comenzarán en orden a jugar y se turnarán para ingresar por pantalla un número N hasta acertar el número oculto y salir ganador.
          
         El juego debe tener la funcionalidad de escoger el número de participantes que jugarán: mínimo 2 y máximo 4 integrantes. Dependiendo de la cantidad de jugadores, el número N aleatorio se generará en los siguientes rangos:
 
-        - Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
-        - Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100
-        - Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
+        - Si participan 2 jugadores, el número N aleatorio se generará entre 0 y 50
+        - Si participan 3 jugadores, el número N aleatorio se generará entre 0 y 100
+        - Si participan 4 jugadores, el número N aleatorio se generará entre 0 y 200
 
         Cada jugador comenzará su turno intentando adivinar ese número ℕ aleatorio. El programa deberá mostrarle al jugador la siguiente información:
 
@@ -22,5 +22,29 @@
         El programa deberá estar en capacidad de pedir a los jugadores si desean un nuevo “tirito” para volver a jugar y borrar consola, de lo contrario, finalizar el programa.*/
 
         //Variables
+        bool JugarDeNuevo = true;
+
+        while (JugarDeNuevo)
+        {
+            Console.Clear(); //Siempre limpiar la consola al comenzar de nuevo
+            Console.WriteLine("Bienvenidos al juego 'Adivina el número'!");
+            Console.WriteLine("¿Cuántos jugadores participaran? (2-4)");
+
+            int Jugadores = 0;
+
+            //Validación para el número de jugadores
+            while (Jugadores < 2 || Jugadores > 4)
+            {
+                if (Jugadores < 2 || Jugadores > 4)
+                {
+                    Console.WriteLine("El número de jugadores debe ser entre 2 y 4");
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Por favor ingrese un número entre 2 y 4.");
+                }
+            }
+
+        }
     }
 }
