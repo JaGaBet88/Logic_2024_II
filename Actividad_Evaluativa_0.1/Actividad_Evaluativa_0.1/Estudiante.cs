0b7dbc57c7733dfa21ae4a4c8c651457deb644d0
Jairo <jairogallegobetancur25@gmail.com>
2024-10-28 20:47:14 -0500

    Clase estudiante | Propiedades

---
+++ Actividad_Evaluativa_0.1/Actividad_Evaluativa_0.1/Estudiante.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_Evaluativa_0._1
+{
+    internal class Estudiante
+    {
+
+        //Propiedades
+        public int Estrato { get; set; }
+        public int Creditos { get; set; }
+        public decimal CostoCredito { get; set; }
+
+        //Constructor
+        public Estudiante(int estrato, int creditos, decimal costoCredito) 
+        {
+            Estrato = estrato;
+            Creditos = creditos;
+            CostoCredito = costoCredito;
+        }
+
+    }
+}
