f82f35433bdec0483fe52f4cad4129629c590ae3
Jairo <jairogallegobetancur25@gmail.com>
2024-10-29 19:04:59 -0500

    Commit metodos

--- Actividad_Evaluativa_0.1/Actividad_Evaluativa_0.1/Estudiante.cs
+++ Actividad_Evaluativa_0.1/Actividad_Evaluativa_0.1/Estudiante.cs
@@ -7,53 +7,80 @@ using System.Threading.Tasks;
 
 namespace Actividad_Evaluativa_0._1
 {
-    internal class Estudiante
+    public class Estudiante
     {
+        // Propiedades
+        public int Estrato { get; set; } // Estrato socioeconómico del estudiante
+        public int Creditos { get; set; } // Cantidad de créditos a tomar
+        public decimal CostoCredito { get; set; } // Costo de cada crédito
 
-        //Propiedades
-        public int Estrato { get; set; }
-        public int Creditos { get; set; }
-        public decimal CostoCredito { get; set; }
-
-        //Constructor
-        public Estudiante(int estrato, int creditos, decimal costoCredito) 
+        // Constructor
+        public Estudiante(int estrato, int creditos, decimal costoCredito)
         {
             Estrato = estrato;
             Creditos = creditos;
             CostoCredito = costoCredito;
         }
 
-        //Método para calcular el costo de la matricula
+        // Método para calcular el costo de la matrícula
         public decimal CalcularCostoMatricula()
         {
             decimal costoTotal;
 
             if (Creditos <= 20)
             {
-                //Si toma 20 créditos, se calcula con el costo normal por crédito
+                // Si toma 20 o menos créditos, se calcula con el costo normal por crédito
                 costoTotal = Creditos * CostoCredito;
             }
             else
             {
-                //Si toma mas de 20 créditos, los créditos adicionales se pagan al doble
-                int CreditosExtra = Creditos - 20;
-                costoTotal = (20 * CostoCredito) + (CreditosExtra * CostoCredito * 2);
+                // Si toma más de 20 créditos, los créditos adicionales se pagan al doble
+                int creditosExtra = Creditos - 20;
+                costoTotal = (20 * CostoCredito) + (creditosExtra * CostoCredito * 2);
             }
 
-            //Resta el descuento correspondiente basado en el estrato
+            // Resta el descuento correspondiente basado en el estrato
             return costoTotal - CalcularDescuento(costoTotal);
+        }
 
-            //Método para calcular el descuento basado en el estrato
-            private decimal CalcularDescuento(decimal costo)
-            {
-                decimal descuento = 0;
+        // Método para calcular el descuento basado en el estrato del estudiante
+        private decimal CalcularDescuento(decimal costo)
+        {
+            decimal descuento = 0;
 
-            switch(Estrato) 
+            switch (Estrato)
             {
                 case 1:
-
-
+                    descuento = costo * 0.80m; // Descuento del 80% para estrato 1
+                    break;
+                case 2:
+                    descuento = costo * 0.50m; // Descuento del 50% para estrato 2
+                    break;
+                case 3:
+                    descuento = costo * 0.30m; // Descuento del 30% para estrato 3
+                    break;
             }
 
+            return descuento;
+        }
+
+        // Método para calcular el subsidio de alimentación y transporte
+        public decimal CalcularSubsidio()
+        {
+            decimal subsidio = 0;
+
+            if (Estrato == 1)
+            {
+                subsidio = 200000; // Subsidio para estrato 1
+            }
+            else if (Estrato == 2)
+            {
+                subsidio = 100000; // Subsidio para estrato 2
+            }
+
+            return subsidio;
+        }
     }
+
 }
+
