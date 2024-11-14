13ae85ff86ff7ca63232a4e85cc315bf9245e023
Jairo <jairogallegobetancur25@gmail.com>
2024-11-13 20:32:29 -0500

    Inicio commit

---
+++ EjercicioArrays/EjercicioArrays/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* 42.Desarrolle un algoritmo donde almacene las ventas que obtuvo mensualmente un almacén durante el año 2023.Mostrar por pantalla cada mes con su respectiva venta en pesos y la suma total de ventas de dicho año.*/
+    }
+}
